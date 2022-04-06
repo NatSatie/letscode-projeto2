3d60fbd139a17752b66df3a7476527ac25f1ec17
Pedro Otávio Freiman Blaudt <pedrofr@outlook.com>
2022-04-06 14:54:51 -0300

    Atualiza foreign keys e modifica tipo percentual

--- sqlSchemas/criacaoTabelas.sql
+++ sqlSchemas/criacaoTabelas.sql
@@ -15,7 +15,7 @@ create table professor (
 create table disciplina (
 	id int not null generated always as identity,
 	nome varchar(50) not null,
-	professor_id int,
+	professor_id int not null,
 	constraint professor_fk foreign key (professor_id) references professor(id),
 	constraint disciplina_pk primary key (id)
 );
@@ -23,8 +23,8 @@ create table disciplina (
 
 create table questao (
 	id int not null generated always as identity,
-	enunciado varchar(120) not null,
-	disciplina_id int,
+	enunciado text not null,
+	disciplina_id int not null,
 	constraint disciplina_fk foreign key (disciplina_id) references disciplina(id),
 	constraint questao_pk primary key (id)
 );
@@ -33,16 +33,16 @@ create table alternativa (
 	id int not null generated always as identity,
 	descricao varchar(50) not null,
 	eh_resposta boolean not null,
-	questao_id int,
+	questao_id int not null,
 	constraint questao_fk foreign key (questao_id) references questao(id),
 	constraint altenativa_pk primary key (id)
 );
 
 create table historico (
 	id int not null generated always as identity,
-	percentual_acerto int not null,
-	disciplina_id int,
-	aluno_id int,
+	percentual_acerto decimal(2) not null,
+	disciplina_id int not null,
+	aluno_id int not null,
 	constraint disciplina_fk foreign key (disciplina_id) references disciplina(id),
 	constraint aluno_fk foreign key (aluno_id) references aluno(id),
 	constraint historico_pk primary key (id)
