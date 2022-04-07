a8bb8cbda0b1cb1151ad9737a8cb33bb2f2c5595
Natália Satie <naty.odashima@gmail.com>
2022-04-07 14:19:48 -0300

    fix: repository issues

--- src/main/java/com/example/letscode/professor/ProfessorRepository.java
+++ src/main/java/com/example/letscode/professor/ProfessorRepository.java
@@ -5,4 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProfessorRepository extends JpaRepository<Professor, Integer> {
+    Professor findProfessorById(Integer id);
 }
--- src/main/java/com/example/letscode/questao/QuestaoRepository.java
+++ src/main/java/com/example/letscode/questao/QuestaoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuestaoRepository extends JpaRepository<Questao, Integer> {
-
+    Questao findQuestaoById(Integer id);
 }
