53088be16b60da8697e08aabdcfa4a8e0d2a3e72
Pedro Otávio Freiman Blaudt <pedrofr@outlook.com>
2022-04-10 18:47:25 -0300

    Add new method for QuestaoRepository

--- src/main/java/com/example/letscode/questao/QuestaoRepository.java
+++ src/main/java/com/example/letscode/questao/QuestaoRepository.java
@@ -1,9 +1,13 @@
 package com.example.letscode.questao;
 
+import com.example.letscode.aluno.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface QuestaoRepository extends JpaRepository<Questao, Integer> {
     Questao findQuestaoById(Integer id);
+    List<Questao> findQuestaoByEnunciadoEquals(String enunciado);
 }
