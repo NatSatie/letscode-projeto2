b9d3178b3ec545b32317457cccaf656c8ee662c1
Pedro Otávio Freiman Blaudt <pedrofr@outlook.com>
2022-04-10 18:46:44 -0300

    Add new methods for AlternativaRepository

--- src/main/java/com/example/letscode/alternativa/AlternativaRepository.java
+++ src/main/java/com/example/letscode/alternativa/AlternativaRepository.java
@@ -1,9 +1,14 @@
 package com.example.letscode.alternativa;
 
+import com.example.letscode.questao.Questao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AlternativaRepository extends JpaRepository<Alternativa, Integer> {
     Alternativa findAlternativaById(Integer id);
+    List<Alternativa> findByDescricaoEquals(String descricao);
+    List<Alternativa> findAlternativaByQuestao(Questao questao);
 }
